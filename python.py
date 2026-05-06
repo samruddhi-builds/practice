bf1bb794d9ac981424e91131b2e396a776f8ecd7
samruddhi-builds <samruddhidave13@gmail.com>
2026-05-06 06:58:17 +0530

    Fix prime number check condition in python.py

--- python.py
+++ python.py
@@ -1,7 +1,7 @@
 num = int(input("Enter the number :"))
   if num>1:
     for i in range(2, int(num**0.5)+1):
-        if num==0:
+        if num % i == 0:
           print("Not a prime number")
           break
         else:
