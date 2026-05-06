1c2991d1bd565fcf56dcd10e31f006486bf63505
samruddhi-builds <samruddhidave13@gmail.com>
2026-05-06 06:59:10 +0530

    Refactor prime number checking in python.py
    Fix indentation and improve prime number checking logic.

--- python.py
+++ python.py
@@ -1,11 +1,15 @@
-num = int(input("Enter the number :"))
-  if num>1:
-    for i in range(2, int(num**0.5)+1):
-        if num % i == 0:
-          print("Not a prime number")
-          break
-        else:
-          print("Prime number ")
+num = int(input("Enter the number: "))
 
-  else:
+if num > 1:
+
+    for i in range(2, int(num**0.5) + 1):
+
+        if num % i == 0:
+            print("Not a prime number")
+            break
+
+    else:
+        print("Prime number")
+
+else:
     print("Not a prime number")
