4c3bd30beff64efa7163a1608da85f82a2fc8505
samruddhi-builds <samruddhidave13@gmail.com>
2026-05-06 06:57:25 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,11 +1,11 @@
 num = int(input("Enter the number :"))
   if num>1:
     for i in range(2, int(num**0.5)+1):
-      if num==0:
-        print("Not a prime number")
-        break
-      else:
-        print("Prime number ")
+        if num==0:
+          print("Not a prime number")
+          break
+        else:
+          print("Prime number ")
 
   else:
     print("Not a prime number")
